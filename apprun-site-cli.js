9017552e04ea79a66a88cae6a9985e2749fcd6ae
Yiyi Sun <yiyisun@gmail.com>
2019-06-01 13:12:25 -0400

    format

--- apprun-site-cli.js
+++ apprun-site-cli.js
@@ -1,36 +1,31 @@
 #!/usr/bin/env node
 
-const cli = require('cac')()
-const degit = require('degit')
-const build = require('./cli-build')
+const cli = require('cac')();
+const degit = require('degit');
+const build = require('./cli-build');
 
-cli.command('build', 'build pages')
+cli
+  .command('build', 'build pages')
   .option('-w, --watch', 'watch the folder')
-  .option('-V, --verbose', 'watch the folder')
+  .option('-V, --verbose', 'show verbose diagnostic information')
   .option('-s, --source [sourceDir]', 'source directory')
   .option('-t, --target [targetDir]', 'target directory')
-  .action((options) => build(options))
+  .action(options => build(options));
 
-cli.command('init [targetDir]', 'initialize project')
+cli
+  .command('init [targetDir]', 'initialize project')
   .option('-r, --repo [repo]', 'source directory')
   .action((targetDir = '.', options) => {
-    const repo = options.repo || 'apprunjs/apprun-starter'
+    const repo = options.repo || 'apprunjs/apprun-starter';
     const emitter = degit(repo, {
       cache: true,
       force: false,
       verbose: true
     });
     emitter.on('info', info => console.log(info.message));
-    emitter.clone(targetDir||'').then(() => console.log('done'))
-  })
+    emitter.clone(targetDir || '').then(() => console.log('done'));
+  });
 
-
-cli.help()
-cli.version('0.3.0')
-cli.parse()
-
-
-
-
-
-
+cli.help();
+cli.version('0.3.0');
+cli.parse();
