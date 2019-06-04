21566b1b2bc47e4b576051c6366f8aa7bb0764f2
Yiyi Sun <yiyisun@gmail.com>
2019-06-03 22:10:03 -0400

    :npm run build

--- src/index-esm.tsx
+++ src/index-esm.tsx
@@ -2,13 +2,13 @@ import app from './_site/index-esm';
 import './components/my-img';
 
 import layout from './layout/index';
-import pages, { links } from './_lib/index-esm';
+import pages from './_lib/index-esm';
 
 const site = {
   title: 'My App',
   element: 'main',
   nav: [],
-  sidebar: links,
+  sidebar: [],
   layout,
   pages
 };
