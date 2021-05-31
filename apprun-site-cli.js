6790c50f93bb4ec6afa511b5a5b68a3d1d9b6071
Yiyi Sun <yiyisun@gmail.com>
2021-05-31 14:20:15 -0400

    add init command

--- apprun-site-cli.js
+++ apprun-site-cli.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const degit = require('degit');
 const startup = require('./index');
+
+program
+  .command('init [targetDir]')
+  .description('initialize project', { targetDir: 'target directory (default: current directory)' } )
+  .option('-r, --repo [repo]', 'repository, default: apprunjs/apprun-starter', 'apprunjs/apprun-starter')
+  .action((targetDir = '.', repo) => {
+    // const emitter = degit(repo, {
+    //   cache: false,
+    //   force: false,
+    //   verbose: true
+    // });
+    // emitter.on('info', info => console.log(info.message));
+    // emitter.clone(targetDir || '').then(() => console.log('done'));
+  });
 
 program
   .command('build <source>')
