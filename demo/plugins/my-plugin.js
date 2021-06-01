f85ca21309857ac13936a14023cac83008f729bc
Yiyi Sun <yiyisun@gmail.com>
2021-06-01 09:16:29 -0400

    add config to app

--- demo/plugins/my-plugin.js
+++ demo/plugins/my-plugin.js
@@ -3,6 +3,5 @@ const chalk = require('chalk');
 const { cyan, yellow, blue, green, magenta, gray, red } = chalk;
 
 app.on('pre-build', async () => {
-  const config = (await app.query('get-global-config'))[0];
-  console.log(green(config.site_name));
+  console.log(green(app.config.site_name));
 });
--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const app = require('apprun').app;
 
 const events = require('./src/events');
 
-let config;
-app.on(events.GET_GLOBAL_CONFIG, () => config);
-
 module.exports = async function (source) {
-  config = yaml.load(fs.readFileSync(`${source}/apprun-site.yml`));
-
+  const config = yaml.load(fs.readFileSync(`${source}/apprun-site.yml`));
   config.pages = `${process.cwd()}/${source}/${config.pages || 'pages'}`;
   config.public = `${process.cwd()}/${source}/${config.public || 'public'}`;
 
@@ -18,17 +14,14 @@ module.exports = async function (source) {
   modules?.forEach(module => require(`${__dirname}/src/${module}`));
   plugins?.forEach(module => require(`${process.cwd()}/${source}/plugins/${module}`));
   const themePath = `${process.cwd()}/${source}/themes/${theme.name}/index.js`;
-  if (fs.existsSync(themePath)) {
-    require(themePath);
-  } else {
-    require(`${__dirname}/src/theme.js`);
-  }
+  config.themePath = fs.existsSync(themePath) ? themePath : `${__dirname}/src/themes/${theme.name}`
 
+  app.config = config;
   if (start) {
-    await app.query(start, config);
+    await app.query(start);
   } else {
-    await app.query(events.PRE_BUILD, config);
-    await app.query(events.BUILD, config);
-    await app.query(events.POST_BUILD, config);
+    await app.query(events.PRE_BUILD);;
+    await app.query(events.BUILD);
+    await app.query(events.POST_BUILD);
   }
 }
