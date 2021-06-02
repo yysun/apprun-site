0a337e550f494456d41f5b42142d6b3d21e7c9e6
Yiyi Sun <yiyisun@gmail.com>
2021-06-02 16:49:53 -0400

    add nav

--- src/themes/bootstrap/layout.js
+++ src/themes/bootstrap/layout.js
@@ -1,9 +1,9 @@
 
-const { title, element, nav } = app['config'];
+const { site_name, site_url, nav } = app['config'];
 
 module.exports = (page) => `<div class="container">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">${title}</a>
+    <a class="navbar-brand" href="/">${site_name}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse"
       data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent"
@@ -12,9 +12,9 @@ module.exports = (page) => `<div class="container">
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        ${nav?.map(item => `<li class="nav-item">
+        ${!nav ? '' : nav.map(item => `<li class="nav-item">
           <a class="nav-link" href=${item.link}>${item.text}</a>
-        </li>`)?.join('')}
+        </li>`).join('')}
       </ul>
     </div>
   </nav>
