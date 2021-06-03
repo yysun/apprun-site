e2de9f1046676d3a1fa7db09e2c5dc254dbbf962
Yiyi Sun <yiyisun@gmail.com>
2021-06-02 22:21:54 -0400

    rename cli.js

--- cli.js
+++ cli.js
@@ -1,72 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
-const chalk = require('chalk');
-const { cyan, yellow, blue, green, magenta, gray, red } = chalk;
+#!/usr/bin/env node
 
-const app = require('apprun').app;
-const events = require('./src/events');
+const program = require('commander');
+const degit = require('degit');
+const build = require('.').build;
 
-async function build({ source, clean, watch, pages, public }) {
-  source = (source && source !== '.') ?
-    path.join(process.cwd(), source) :
-    process.cwd();
+program
+  .command('init [targetDir]')
+  .description('initialize project', { targetDir: 'target directory (default: current directory)' } )
+  .option('-r, --repo [repo]', 'repository, default: apprunjs/apprun-starter', 'apprunjs/apprun-starter')
+  .action((targetDir = '.', repo) => {
+    const emitter = degit(repo, {
+      cache: false,
+      force: false,
+      verbose: true
+    });
+    emitter.on('info', info => console.log(info.message));
+    emitter.clone(targetDir || '').then(() => console.log('done'));
+  });
 
-  const conf = `${source}/apprun-site.yml`;
-  if (!fs.existsSync(conf)) {
-    console.log(red('Cannot read ', conf));
-    return;
-  }
-  const config = yaml.load(fs.readFileSync(conf));
-  config.source = source;
-  config.pages = path.join(source, pages || 'pages');
-  config.public = path.join(source, public || 'public');
-  config.clean = clean;
-  config.watch = watch;
-  const { plugins, start, theme, site_url, tabs } = config;
+program
+  .command('build [source]')
+  .description('build site')
+  .option('-c, --clean', 'clean the output directory', false)
+  .option('-w, --watch', 'watch the directory', false)
+  .option('-p, --public [public]', 'public directory', 'public')
+  .action((source, options) => build({source, ...options}));
 
-  // nav
-  const base = site_url || '/';
-  config.nav = tabs ?
-    Object.keys(tabs).map(key => ({
-      link: `${base}${tabs[key] || ''}`,
-      text: key
-    }))
-    : null;
-  app.config = config;
-
-  // system modules
-  // modules?.forEach(module => require(`${__dirname}/src/${module}`));
-  require('./src/build');
-  require('./src/build-md');
-  require('./src/build-html');
-  require('./src/build-ts');
-  require('./src/build-esm');
-
-  // plugins
-  plugins?.forEach(module => require(`${source}/plugins/${module}`));
-
-  // theme
-  app.get_theme_view = name => {
-    const cust_themeView = path.join(source, "themes", theme.name, `${name}.js`);
-    const sys_themeView = path.join(__dirname, "src", "themes", theme.name, `${name}.js`);
-    if (fs.existsSync(cust_themeView)) {
-      console.log(yellow('Using custom view'), cust_themeView.replace(process.cwd(), '').substr(1));
-      return require(cust_themeView);
-    } else if (!fs.existsSync(sys_themeView)) {
-      console.log(red(`Err: Cannot find theme view: ${theme.name}/${name}`));
-    } else {
-      return require(sys_themeView);
-    }
-  }
-
-  if (start) {
-    await app.query(start);
-  } else {
-    await app.query(events.PRE_BUILD);
-    await app.query(events.BUILD);
-    await app.query(events.POST_BUILD);
-  }
-}
-
-module.exports = { build }
+program.parse(process.args);
