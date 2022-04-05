a03ed5bcdb78e6257440e9ed11175dd66166c961
Yiyi Sun <yiyisun@gmail.com>
2022-04-05 16:08:21 -0400

    update init command

--- apprun-site-cli.js
+++ apprun-site-cli.js
@@ -6,18 +6,26 @@ import build from './index.js';
 import server from './server.js';
 
 program
-  .command('init [targetDir]')
-  .description('initialize project', { targetDir: 'target directory (default: current directory)' } )
+  .command('init')
+  .description('initialize a new app')
+  .argument('[targetDir]', 'target directory (default: current directory)')
   .option('-r, --repo [repo]', 'repository, default: apprunjs/apprun-starter', 'apprunjs/apprun-starter')
   .action((targetDir = '.', { repo }) => {
-    console.log(repo)
+
+  console.log('Cloning from: ', repo);
     const emitter = degit(repo, {
       cache: false,
       force: false,
       verbose: true
     });
     emitter.on('info', info => console.log(info.message));
-    emitter.clone(targetDir || '').then(() => console.log('done'));
+    emitter.clone(targetDir || '').then(() => {
+      console.log('Project cloned to: ', targetDir);
+      console.log('Please go to the project directory and run:')
+      console.log('\tnpm install')
+      console.log('\tnpm start')
+      console.log('And then, you can visit the project at: http://localhost:8080');
+    });
   });
 
 program
