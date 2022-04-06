82347af396249d0844c30fd00225c2a8ae22c2ff
Yiyi Sun <yiyisun@gmail.com>
2022-04-06 09:14:50 -0400

    remove nodmon

--- src/build.js
+++ src/build.js
@@ -1,6 +1,8 @@
 //@ts-check
 import { existsSync, mkdirSync, readFileSync, rmSync, readdirSync, statSync, writeFileSync, copyFileSync } from 'fs';
 import { join, dirname, basename, extname } from 'path';
+import chokidar from 'chokidar';
+import _ from 'lodash';
 import { load } from 'js-yaml';
 import chalk from 'chalk';
 const { cyan, yellow, blue, green, magenta, gray, red } = chalk;
@@ -37,11 +39,24 @@ app.on(PRE_BUILD, ({ source, clean, output }) => {
   }
 });
 
-app.on(POST_BUILD, async ({ render, output, pages}) => {
+app.on(POST_BUILD, async ({ watch, render, output, pages}) => {
   !config['no-startup'] && app.run(`${BUILD}:startup`, config, output, pages);
   console.log(cyan('Created File'), relative(`${output}/main.js`));
   render && await app.query(`${BUILD}:render`, config, output);
   console.log(cyan('Build done.'));
+
+  if (watch) {
+    console.log(cyan('Watching ...'));
+
+    chokidar.watch(pages).on('all', _.debounce((event, path) => {
+      if (event === 'change'|| event === 'add') {
+        console.log(cyan('Change detected'), relative(path));
+        process_file(path, { pages, output });
+      }
+    },500));
+  } else {
+    console.log(cyan('Build done.'))
+  }
 })
 
 app.on(BUILD, async ({ pages, output }) => {
@@ -57,51 +72,51 @@ app.on(BUILD, async ({ pages, output }) => {
       isDirectory ? walkDir(dirPath, callback) : callback(join(dir, f));
     });
   }
-  walkDir(pages, file => process_file(file));
+  walkDir(pages, file => process_file(file, { pages, output }));
+});
 
+async function process_file(file, { pages, output }) {
+  const dir = dirname(file).replace(pages, '');
+  const name = basename(file).replace(/\.[^/.]+$/, '');
+  const ext = extname(file);
+  // const event = content_events?.[ext] || ext;
+  const event = ext;
+  const pub_dir = join(output, dir);
+  ensure(pub_dir);
 
-  async function process_file(file) {
-    const dir = dirname(file).replace(pages, '');
-    const name = basename(file).replace(/\.[^/.]+$/, '');
-    const ext = extname(file);
-    // const event = content_events?.[ext] || ext;
-    const event = ext;
-    const pub_dir = join(output, dir);
-    ensure(pub_dir);
+  const js_file = join(output, dir, name) + '.js';
 
-    const js_file = join(output, dir, name) + '.js';
+  // console.log('Page: ', file, '=>', event);
+  const text = readFileSync(file).toString();
 
-    // console.log('Page: ', file, '=>', event);
-    const text = readFileSync(file).toString();
+  if (HTML_Types.indexOf(ext) >= 0 && text.indexOf('<html') >= 0) {
+    const new_file = join(pub_dir, name + '.html');
+    writeFileSync(new_file, text);
+    console.log(cyan('Copied HTML'), relative(new_file));
+    return;
+  }
 
-    if (HTML_Types.indexOf(ext) >= 0 && text.indexOf('<html') >= 0) {
-      const new_file = join(pub_dir, name + '.html');
-      writeFileSync(new_file, text);
-      console.log(cyan('Copied HTML'), relative(new_file));
+  if (Content_Types.indexOf(ext) >= 0) {
+    const all_content = await app.query(`${BUILD}${event}`, text);
+    const content = last(all_content);
+    // console.log(content);
+    if (!content) {
+      console.log(red('Content load failed'));
       return;
     }
-
-    if (Content_Types.indexOf(ext) >= 0) {
-      const all_content = await app.query(`${BUILD}${event}`, text);
-      const content = last(all_content);
-      // console.log(content);
-      if (!content) {
-        console.log(red('Content load failed'));
-        return;
-      }
-      app.run(`${BUILD}:component`, content, js_file, output);
-      app.run(`${BUILD}:add-route`, dir, js_file, output);
-      console.log(cyan('Created Component'), relative(js_file));
-    } else if (Esbuild_Types.indexOf(ext) >= 0) {
-      app.run(`${BUILD}:esbuild`, file, js_file, output);
-      console.log(cyan('Compiled JavaSript'), relative(js_file));
-      app.run(`${BUILD}:add-route`, dir, js_file, output);
-    } else if (Copy_Types.indexOf(ext) >= 0) {
-      const dest = join(pub_dir, name) + ext;
-      copyFileSync(file, dest);
-      console.log(cyan('Copied File'), relative(dest));
-    } else {
-      console.log(magenta('Unknown file type'), relative(file));
-    }
+    app.run(`${BUILD}:component`, content, js_file, output);
+    app.run(`${BUILD}:add-route`, dir, js_file, output);
+    console.log(cyan('Created Component'), relative(js_file));
+  } else if (Esbuild_Types.indexOf(ext) >= 0) {
+    app.run(`${BUILD}:esbuild`, file, js_file, output);
+    console.log(cyan('Compiled JavaSript'), relative(js_file));
+    app.run(`${BUILD}:add-route`, dir, js_file, output);
+  } else if (Copy_Types.indexOf(ext) >= 0) {
+    const dest = join(pub_dir, name) + ext;
+    copyFileSync(file, dest);
+    console.log(cyan('Copied File'), relative(dest));
+  } else {
+    console.log(magenta('Unknown file type'), relative(file));
   }
-});
+}
+
