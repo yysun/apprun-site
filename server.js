0b9c12baf8919cd6a204e2cfc5a0a71a750651ba
Yiyi Sun <yiyisun@gmail.com>
2024-08-29 23:20:09 -0400

    add error handling to server.js

--- server.js
+++ server.js
@@ -26,73 +26,81 @@ export default function (config = {}) {
 
 export function ssr(app, root, no_ssr, save_ssr) {
 
-  app.get('*', async (req, res) => {
-    let path = req.path;
-    if (path.includes('.')) {
-      res.sendFile(path, { root });
-    } else {
-      if (!path.endsWith('/')) path += '/';
-      const html_file = `${root}${path}index.html`;
-      console.log(cyan(`Serving ${path}`));
-      if (existsSync(html_file)) {
-        console.log(cyan(`\t${html_file}`));
-        res.sendFile(html_file);
-      }
-      else if (no_ssr) {
-        const home_html = `${root}/index.html`;
-        console.log(gray(`\t${home_html} (SPA)`));
-        res.sendFile(home_html);
+  app.get('*', async (req, res, next) => {
+    try {
+      let path = req.path;
+      if (path.includes('.')) {
+        res.sendFile(path, { root });
       } else {
-        let content = await render(path, root);
-        if (content) {
-          if (save_ssr) {
-            writeFileSync(html_file, content);
-          }
-          console.log(green(`\t${root}${path} - SSR`));
-          res.send(content);
+        if (!path.endsWith('/')) path += '/';
+        const html_file = `${root}${path}index.html`;
+        console.log(cyan(`Serving ${path}`));
+        if (existsSync(html_file)) {
+          console.log(cyan(`\t${html_file}`));
+          res.sendFile(html_file);
+        }
+        else if (no_ssr) {
+          const home_html = `${root}/index.html`;
+          console.log(gray(`\t${home_html} (SPA)`));
+          res.sendFile(home_html);
         } else {
-          console.log(red(`\t${path} not found`));
-          res.sendStatus(404);
+          let content = await render(path, root);
+          if (content) {
+            if (save_ssr) {
+              writeFileSync(html_file, content);
+            }
+            console.log(green(`\t${root}${path} - SSR`));
+            res.send(content);
+          } else {
+            console.log(red(`\t${path} not found`));
+            res.sendStatus(404);
+          }
         }
       }
+    }
+    catch (err) {
+      next(err);
     }
   });
 }
 
 export function api(app, source) {
   app.get('/api/*', async (req, res, next) => {
-    const run_api = async (js_file) => {
-      const { mtimeMs } = statSync(js_file);
-      const module = await import(`file://${js_file}?${mtimeMs}`);
-      const exp = module.default;
-      console.log(blue(`\t${js_file}`));
-      exp(req, res, next);
-    };
+    try {
+      const run_api = async (js_file) => {
+        const { mtimeMs } = statSync(js_file);
+        const module = await import(`file://${js_file}?${mtimeMs}`);
+        const exp = module.default;
+        console.log(blue(`\t${js_file}`));
+        exp(req, res, next);
+      };
 
-    const path = req.path;
-    const paths = path.split('/');
-    console.log(blue(`Serving API ${path}`));
-    for (let i = paths.length; i > 1; i--) {
-      const route = paths.slice(1, i).join('/');
-      let js_index = `${source}/${route}/index.js`;
-      let js_file = `${source}/${route}.js`;
-      try {
+      const path = req.path;
+      const paths = path.split('/');
+      console.log(blue(`Serving API ${path}`));
+      for (let i = paths.length; i > 1; i--) {
+        const route = paths.slice(1, i).join('/');
+        let js_index = `${source}/${route}/index.js`;
+        let js_file = `${source}/${route}.js`;
+
         if (existsSync(js_file)) {
           await run_api(js_file);
-          return;
+          next();
         } else if (existsSync(js_index)) {
           await run_api(js_index);
-          return;
+          next();
         }
-      } catch (e) {
-        console.log(red(e.message));
-        res.sendStatus(500);
-        return;
-        // } finally {
-        //   delete require.cache[js_file];
       }
+      console.log(magenta(`\tUnknown path ${path}`));
+      res.sendStatus(404);
+    } catch (e) {
+      console.log(red(e.message));
+      next(e);
     }
-    console.log(magenta(`\tUnknown path ${path}`));
-    res.sendStatus(404);
+  });
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error
+    res.status(500).send(err.message); // Send a generic error response
   });
 }
