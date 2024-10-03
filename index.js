fc65a245033a7fe7e62812aeb92d22f4e081c2d4
Yiyi Sun <yiyisun@gmail.com>
2024-10-03 12:12:32 -0400

    export server and dev server

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+/**
+ * @fileoverview Entry point for the apprun-site.
+ *
+ * This module exports the build, server and devServer functionalities.
+ *
+ * @module
+ */
+
 //@ts-check
 import build from './src/build.js';
-import server from './dev-server.js';
+import devServer from './dev-server.js';
+import server from './server.js';
 
-export { build, server };
+export { build, server, devServer };
